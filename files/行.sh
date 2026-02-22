63a8f19e61158f68a2eb872fb63ef62ebd3357f1
bediha877nb <bediha877@gmail.com>
2026-02-22 18:41:09 +0800

    Add files via upload

---
+++ files/行.sh
@@ -0,0 +1,14 @@
+# 初始化 Git 仓库
+git init
+
+# 关联你的远程仓库（替换成你的仓库地址）
+git remote add origin https://github.com/bediha877nb/gejiwenjian.git
+
+# 添加所有文件
+git add .
+
+# 提交
+git commit -m "add files folder"
+
+# 推送到 GitHub
+git push -u origin main
